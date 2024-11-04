d310f15bac6ee861ba00d869013f7d7b18d74f30
AnnaSem22 <plombamexx@gmail.com>
2024-11-04 20:26:34 +0300

    1

---
+++ ShoppingCenter/purchase.java
@@ -0,0 +1,11 @@
+package ShoppingCenter;
+/*
+Product No 1: smartphone,
+total sales for 5 days is EUR 12153,41,
+sales by day is EUR 2430,68.
+Product No 2: laptop,
+total sales for 7 days is EUR 10486,85,
+sales by day is EUR 1498,12.
+*/
+public class purchase {
+}
